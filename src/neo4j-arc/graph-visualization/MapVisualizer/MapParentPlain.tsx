c79ae54194d8a1e4a33f642d763ffc66dc718da2
eisserer <clemens.eisserer@extern.umweltbundesamt.at>
2024-06-10 13:54:48 +0200

    remove invalid EPSG:3035 definition

--- src/neo4j-arc/graph-visualization/MapVisualizer/MapParentPlain.tsx
+++ src/neo4j-arc/graph-visualization/MapVisualizer/MapParentPlain.tsx
@@ -275,11 +275,6 @@ function registerProjections() {
     '+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
   )
 
-  /*proj4.defs(
-    'EPSG:3035',
-    '+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=565.04,49.91,465.84,1.9848,-1.7439,9.0587,4.0772 +units=m +no_defs +type=crs'
-  )*/
-
   proj4.defs(
     'EPSG:3857',
     '+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs'
